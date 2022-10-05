4c6010463063cf672475a381fc41902d59993988
Sarah Richnavsky <112355091+SarahRichnavsky@users.noreply.github.com>
2022-10-05 23:02:11 +0000

    asdf

--- main.cpp
+++ main.cpp
@@ -1,27 +1,3 @@
-
-//?  [Problem Definition]
-//*     Write a program that asks for the weight of the package and
-//*     the distance it is to beshipped, and then displays the charges.
-//** Input Validation :
-//* 	Do not accept values of 0 or less for the weight of the package.
-//* 	Do not accept weights of more than 20 kg (this is the maximum weight the company will ship).
-//* 	Do not accept distances of less than 10 miles or more than 3,000 miles.
-//*     These are the company’s minimum and maximum shipping distances.
-
-// Program Outline
-// Input
-// 	Two Integers, weight and distance.  ( 0< weight <= 20, 10 <= distance < 3000)
-// Output
-// 	the Charges, Weight,  and Distances
-// Program Logic
-// 	Input statement for user input
-// 	make the validation statement for the input value
-// 	make the decision structure to determine the rate based on weight
-// 	calculate the total charges
-// 		if distance > 500, ( charges = (distance / 500) * rate);
-// 		else, charges = rate;
-// 	print out the charge, distance, weight.
-
 #include <iostream>
 using namespace std;
 int main(){
@@ -46,7 +22,7 @@ else if (weight<6)
   dRate = RATE6;
 else if (weight<10)
   dRate = RATE10;
-else if (weight<20)
+else if (weight<=20)
   dRate = RATE20;
 
 else {
